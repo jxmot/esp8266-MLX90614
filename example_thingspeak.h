9189ce7eb82956fdf38d4fbfcb6bea332538ee7d
jxmot <webdev@stupiddog.net>
2017-12-31 02:28:58 -0600

    thingspeak configuration, initial commit

---
+++ example_thingspeak.h
@@ -0,0 +1,8 @@
+
+// Thingspeak Settings - Fill in with the information for the
+// channel you've created at Thingspeak...
+const int HTTPPORT = 80;
+const String THINGSPEAK_CHANNEL_ID = "";
+const String THINGSPEAK_API_READ_KEY = "";
+const char* THINGSPEAK_API_WRITE_KEY = "";
+const char* THINGSPEAK_HOST = "";
